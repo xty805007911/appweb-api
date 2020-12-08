1aed2e590c2e58186831e8ab5518862dba930071
肖天宇 <5180179+pastthewind@user.noreply.gitee.com>
2020-12-08 22:14:18 +0800

    修改为模板引擎

--- src/main/java/com/ctsi/controller/CommonController.java
+++ src/main/java/com/ctsi/controller/CommonController.java
@@ -5,13 +5,14 @@ import com.ctsi.service.UserService;
 import com.ctsi.util.CookieUtils;
 import com.ctsi.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @ClassName : CommonController
@@ -19,21 +20,49 @@ import javax.servlet.http.HttpServletResponse;
  * @Author : Xiaotianyu  //作者
  * @Date: 2020-12-07 17:36
  */
-@RestController
+@Controller
 public class CommonController {
 
     @Autowired
     UserService userService;
 
-    @PostMapping("/user/login")
-    public Result login(@RequestBody TbUser formUser) {
-        TbUser user = userService.getUserByMobileAndPassword(formUser.getMobile(), formUser.getPassword());
-        return Result.ok(user);
+    //首页
+    @RequestMapping("/")
+    public String index(HttpServletRequest request) {
+        Map<String,Object> map = new HashMap<>();
+        map.put("message","测试成功");
+        request.setAttribute("result",map);
+        return "/tmp";
     }
 
+    //登录页跳转
+    @RequestMapping("/toLogin")
+    public String login(HttpServletRequest request) {
+        Map<String,Object> map = new HashMap<>();
+        map.put("error","");
+        request.setAttribute("result",map);
+        return "/login";
+    }
+    //用户登录
+    @PostMapping("/user/login")
+    public String userLogin(HttpServletRequest request, HttpSession session, TbUser formUser) {
+        TbUser user = userService.getUserByMobileAndPassword(formUser.getMobile(), formUser.getPassword());
+
+        if(user == null || user.getId() == null) {
+            Map<String,Object> map = new HashMap<>();
+            map.put("error","mobile or password error.");
+            request.setAttribute("result",map);
+            return "/login";
+        }
+
+        session.setAttribute("sessionUser",user);
+        return "redirect:/";
+    }
+    //退出
     @GetMapping("/user/logout")
-    public Result logout() {
-        return Result.ok();
+    public String logout(HttpSession session) {
+        session.removeAttribute("sessionUser");
+        return "redirect:/";
     }
 
     @GetMapping("/user/info")
