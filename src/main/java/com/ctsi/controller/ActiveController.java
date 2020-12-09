dbee5485eb9f0b81559ac752e845b79e74916a0f
xiaotianyu <xiaotianyu@bjfu.edu.cn>
2020-12-09 18:43:39 +0800

    添加实体

--- src/main/java/com/ctsi/controller/ActiveController.java
+++ src/main/java/com/ctsi/controller/ActiveController.java
@@ -1,6 +1,17 @@
 package com.ctsi.controller;
 
+import com.ctsi.entity.TbActive;
+import com.ctsi.service.TbActiveService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * @ClassName : ActiveController
@@ -10,4 +21,24 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class ActiveController {
+    @Autowired
+    TbActiveService activeService;
+
+    //添加活动
+    @RequestMapping("/active/add")
+    public String addActive(HttpServletRequest request, TbActive active) {
+        activeService.add(active);
+        return "rediect:/active/pageList";
+    }
+
+    //分页条件查询
+    @RequestMapping("/active/pageList")
+    public String activePageList(@RequestParam(required = false) TbActive active, HttpServletResponse response,HttpServletRequest request) throws IOException {
+
+
+
+        return "/activemanage/active-list";
+    }
+
+
 }
--- src/main/java/com/ctsi/service/TbActiveService.java
+++ src/main/java/com/ctsi/service/TbActiveService.java
@@ -1,8 +1,19 @@
 package com.ctsi.service;
 
+import com.ctsi.entity.TbActive;
+import com.ctsi.mapper.TbActiveMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TbActiveService {
+
+    @Autowired
+    TbActiveMapper activeMapper;
+
+    //添加活动
+    public void add(TbActive active) {
+        activeMapper.insert(active);
+    }
 
 }
